0f7ada6e75b075e89f1e1f00e390ed33e664d11d
agncdel <167378281+agncdel@users.noreply.github.com>
2024-05-14 22:39:46 -0300

    Mal subido

--- src/ar/edu/unalm/inmobiliaria/Casa.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unalm.inmobiliaria;
-
-public class Casa extends Propiedad {
-
-	public Casa(Integer idCasa, String calle, Integer numero, String localidad, Double precio, Boolean estaDisponible,
-			Cliente dueño) {
-		super(idCasa, calle, numero, localidad, precio, estaDisponible, dueño);
-	}
-}
